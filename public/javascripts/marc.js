58c201bb9be9ea203af9cf03756c0e2731aa4407
Aaron Wright <acwright@acwrightdesign.com>
2011-05-16 14:26:18 -0500

    Added rollover image swap.

---
+++ public/javascripts/marc.js
@@ -0,0 +1,16 @@
+/* Marc */
+
+var marc = {
+  
+  run: function() {
+    
+    $('ul#categories a').mouseover(function(event) {
+      $('img#categories').attr('src', $(this).attr('data-image-url'));
+    });
+    
+    $('ul#products a').mouseover(function(event) {
+      $('img#products').attr('src', $(this).attr('data-image-url'));
+    });
+  }
+  
+};
