2a0b382a2a9d81665dbf0b5d18d279c9b4a99208
Aaron Wright <acwright@acwrightdesign.com>
2011-05-18 04:28:43 -0500

    Fixes for invalid HTML.

--- public/javascripts/marc.js
+++ public/javascripts/marc.js
@@ -5,11 +5,11 @@ var marc = {
   run: function() {
     
     $('ul#categories a').mouseover(function(event) {
-      $('img#categories').attr('src', $(this).attr('data-image-url'));
+      $('img.categories').attr('src', $(this).attr('data-image-url'));
     });
     
     $('ul#products a').mouseover(function(event) {
-      $('img#products').attr('src', $(this).attr('data-image-url'));
+      $('img.products').attr('src', $(this).attr('data-image-url'));
     });
   }
   
