00e3acd42eb3fd98335e9f6d528683a32663ba95
Aaron Wright <acwright@acwrightdesign.com>
2011-05-18 12:21:53 -0500

    Fixed home page bug.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   
   def index
-    @featured_product = Product.where('featured = ? AND active = ?', true, true)
-    @featured_product = Product.first(:offset => rand(Product.count)) if @featured_product.empty?
+    @featured_product = Product.where('featured = ? AND active = ?', true, true).first
+    @featured_product = Product.first(:offset => rand(Product.count)) if @featured_product.nil?
   end
 end
