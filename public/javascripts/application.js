2c6a9c2cad96601c3bba7c19542f07c3bf521d2e
Aaron Wright <acwright@acwrightdesign.com>
2011-06-06 17:29:17 -0500

    Added home page banner.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,17 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+var marc = {
+  
+  run: function() {
+    
+    $('ul.categories a').mouseover(function(event) {
+      $('img.categories').attr('src', $(this).attr('data-image-url'));
+    });
+    
+    $('ul.products a').mouseover(function(event) {
+      $('img.products').attr('src', $(this).attr('data-image-url'));
+    });
+  }
+  
+};
