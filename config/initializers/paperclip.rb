a472c7ad9251d44a7cfffcd08794b53f693e4747
Aaron Wright <acwrightdesign@gmail.com>
2015-02-09 15:56:36 -0600

    Update

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,2 @@
 #Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-Paperclip::Attachment.default_options[:path] = '/app/public/system/:attachment/:id_partition/:style/:filename'
+Paperclip::Attachment.default_options[:path] = '/app/public/system/:attachment/:id/:style/:filename'
